9635377d594e926c5af6b98002b86787872a551c
jeevaphysicist <jeevaphysicist@gmail.com>
2023-01-25 11:35:44 +0530

    code submission

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+// import Next from "./Components/Next";
+import Home from "../src/Components/HomePages/Home";
+import {Routes,Route} from "react-router-dom";
+import Chat from "./Components/Chat/Chat";
+import Mobilechat from "./Components/Chat/Mobilechat";
+
+
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
+
+
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        {/* <Route  path="/chat" element={<Chat/>} />
+        <Route path="/mobile" element={<Mobilechat/>} /> */}
+      </Routes>
+     
+     
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ src/Components/APi.js
@@ -0,0 +1,3 @@
+// const API = "https://easychatnew.herokuapp.com";
+const API = "http://localhost:8080";
+module.exports = API;
